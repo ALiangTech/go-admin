b2a246e64eca2488d651321f064cc87fe9e1684b
平平无奇的阿良 <40046221+ALiangTech@users.noreply.github.com>
2023-09-20 18:06:35 +0800

    🚧 新增错误校验

--- routers/v2/login/index.go
+++ routers/v2/login/index.go
@@ -4,6 +4,8 @@ import (
 	"aliangtect/go-admin/db"
 	"aliangtect/go-admin/utils"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -28,6 +30,9 @@ func HanderLogin(router *gin.RouterGroup) {
 	router.POST("/login", func(ctx *gin.Context) {
 		form := Form{}
 		if err := ctx.ShouldBind(&form); err == nil {
+			message := validateInputNotEmpty(form)
+			fmt.Println(message)
+			fmt.Println("message")
 			db.DB.Raw("SELECT (pwd = crypt(?, pwd)) AS pswmatch FROM users where name = ?;", form.Pwd, form.Name).Scan(&users)
 			token, err := utils.GenerateJwt()
 			fmt.Printf("%v err", err)
@@ -38,4 +43,24 @@ func HanderLogin(router *gin.RouterGroup) {
 			"message": "pong3",
 		})
 	})
+}
+
+// 随机返回 账号 密码 中文字符串 顺序
+func randName() (string, string) {
+	const size = 2
+	messageSet := [size]string{"账号", "密码"}
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+	firstIndex := random.Intn(size) // 0 1
+	lastIndex := size - firstIndex - 1
+	return messageSet[firstIndex], messageSet[lastIndex]
+}
+
+// 检测用户输入的数据是否为空
+func validateInputNotEmpty(form Form) string {
+	if len(form.Name) == 0 || len(form.Pwd) == 0 { // 说明用户输入的数据 有问题
+		m1, m2 := randName()
+		return fmt.Sprintf("%s或%s错误", m1, m2)
+	}
+	return ""
 }
