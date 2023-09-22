ff665482c2977543f58a5f6d5a7cad4d7f70f607
平平无奇的阿良 <40046221+ALiangTech@users.noreply.github.com>
2023-09-22 18:10:36 +0800

    🎨 优化登录接口

--- routers/v2/login/index.go
+++ routers/v2/login/index.go
@@ -3,8 +3,10 @@ package login
 import (
 	"aliangtect/go-admin/db"
 	"aliangtect/go-admin/utils"
+	"errors"
 	"fmt"
 	"math/rand"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -26,53 +28,33 @@ var users Users
 
 func HanderLogin(router *gin.RouterGroup) {
 	router.POST("/login", func(ctx *gin.Context) {
-		form := Form{}
-		// 有错误 但是我依旧会返回基础结构给你 但是数据肯定是空
-		// 没有错误 就正常返回数据
-		if validateRequestContentType(gin.MIMEJSON, ctx) {
-			// 请求体 content-type 类型类型正确
-			// 请求体参数校验 字段个数 字段值类型
-			err := parseBody(ctx, &form)
-			if err := ctx.ShouldBindJSON(&form); err == nil {
-				message := validateInputNotEmpty(form)
-				if len(message) != 0 { // 说明存在错误信息
-					ctx.JSON(200, gin.H{
-						"code":    1,
-						"message": message,
-						"data":    "",
-					})
-				} else {
-					validateInputPwd(form, &users)
-					if users.Pswmatch { // 密码正确
-						token, _ := utils.GenerateJwt()
-						ctx.JSON(200, gin.H{
-							"code":    200,
-							"message": "",
-							"data": gin.H{
-								"toke": token,
-							},
-						})
-					} else {
-						ctx.JSON(200, gin.H{
-							"code":    1,
-							"message": createMessage(),
-							"data":    "",
-						})
-					}
+		// form := Form{}
+		validContentType := &ValidContentType{}
 
-				}
-			} else {
-				fmt.Println(err)
-				fmt.Println("err")
-			}
-		} else {
-			ctx.JSON(200, gin.H{
-				"code":    1,
-				"message": fmt.Sprintf("请求体内容类型(content-type)错误,接口只能处理%v类型", gin.MIMEJSON),
-				"data":    "",
-			})
-		}
+		validBody := &ValidBody{}
+		validContentType.SetNext(validBody)
+
+		validBodyCorrectness := &ValidBodyCorrectness{}
+		validBody.SetNext(validBodyCorrectness)
+
+		createJwt := &CreateJwt{}
+		validBodyCorrectness.SetNext(createJwt)
+
+		validContentType.Execute(ctx)
 	})
+}
+
+// 通过责任链 清除嵌套式if else 结构
+
+// 校验基础结构
+type Valid interface {
+	Execute(*gin.Context)
+	SetNext(Valid)
+}
+
+// 校验请求体content-type 是否符合要求
+type ValidContentType struct {
+	next Valid
 }
 
 /**
@@ -83,26 +65,54 @@ func HanderLogin(router *gin.RouterGroup) {
 * 返回值：
 *  - 如果请求content-type 与 目标 相匹配, 则返回true; 否则返回false
  */
-func validateRequestContentType(target string, ctx *gin.Context) bool {
+func validateRequestContentType(ctx *gin.Context, target string) bool {
 	requestContentType := ctx.ContentType()
 	return requestContentType == target
 }
-
-/**
-* 解析请求体里面的body数据
-* 参数：
-*	  - ctx: Gin 上下文对象，用于处理 HTTP 请求。
-*   - form: 指向表单结构体 (*Form) 的指针，用于存储解析后的数据。
-* 返回值:
-*   - error: 如果解析和绑定过程中出现错误，则返回一个描述错误的错误对象；否则返回 nil。
- */
-func parseBody(ctx *gin.Context, form *Form) error {
-	err := ctx.ShouldBindJSON(form)
-	return err
+func (vc *ValidContentType) Execute(ctx *gin.Context) {
+	isValidContentType := validateRequestContentType(ctx, gin.MIMEJSON)
+	if isValidContentType {
+		vc.next.Execute(ctx)
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status":  200,
+			"message": "请求体内容类型错误",
+			"data":    "",
+		})
+	}
+}
+func (vc *ValidContentType) SetNext(next Valid) {
+	vc.SetNext(next)
 }
 
-func response(ctx *gin.Context) {
+// 校验请求体body 数据字段是否完整
 
+type ValidBody struct {
+	next Valid
+}
+
+func (vb *ValidBody) Execute(ctx *gin.Context) {
+	form := Form{}
+	err := ctx.ShouldBindJSON(form)
+	if err == nil {
+		vb.next.Execute(ctx)
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status":  200,
+			"message": fmt.Sprintf("发送的数据：%v", err),
+			"data":    "",
+		})
+	}
+}
+
+func (vb *ValidBody) SetNext(next Valid) {
+	vb.next = next
+}
+
+// ** 校验请求body 数据值是否正确
+
+type ValidBodyFieldValue struct {
+	next Valid
 }
 
 // 随机返回 账号 密码 中文字符串 顺序
@@ -123,15 +133,73 @@ func createMessage() string {
 }
 
 // 检测用户输入的数据是否为空
-func validateInputNotEmpty(form Form) string {
+func validateInputNotEmpty(form Form) (string, error) {
 	if len(form.Name) == 0 || len(form.Pwd) == 0 { // 说明用户输入的数据 有问题
-		return createMessage()
+		return createMessage(), errors.New("数据值有问题")
 	}
-	return ""
+	return "", nil
+}
+func (vf *ValidBodyFieldValue) Execute(ctx *gin.Context) {
+	form := Form{}
+	msg, err := validateInputNotEmpty(form)
+	if err == nil {
+		vf.next.Execute(ctx)
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status":  200,
+			"message": fmt.Sprintf("请求数据值：%v", msg),
+			"data":    "",
+		})
+	}
+}
+
+// 校验请求体数据 正确性
+
+type ValidBodyCorrectness struct {
+	next Valid
 }
 
 // 执行sql 校验密码是否正确 并查找用户
-
 func validateInputPwd(form Form, users *Users) {
 	db.DB.Raw("SELECT (pwd = crypt(?, pwd)) AS pswmatch FROM users where name = ?;", form.Pwd, form.Name).Scan(users)
+}
+
+func (vbc *ValidBodyCorrectness) Execute(ctx *gin.Context) {
+	form := Form{}
+	validateInputPwd(form, &users)
+	if users.Pswmatch {
+		vbc.next.Execute(ctx)
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status":  200,
+			"message": createMessage(),
+			"data":    "",
+		})
+	}
+}
+
+func (vbc *ValidBodyCorrectness) SetNext(next Valid) {
+	vbc.next = next
+}
+
+// 创建jwt
+
+type CreateJwt struct {
+	next Valid
+}
+
+func (cj *CreateJwt) Execute(ctx *gin.Context) {
+	jwt, err := utils.GenerateJwt()
+	if err == nil {
+		fmt.Println(jwt)
+	} else {
+		ctx.JSON(http.StatusOK, gin.H{
+			"status":  200,
+			"message": "登录失败jwt",
+			"data":    "",
+		})
+	}
+}
+func (cj *CreateJwt) SetNext(next Valid) {
+	cj.next = next
 }
