81b188de5b99d6f524b0bf08d0e1911afa8475a7
AliangTech <seminapater@gmail.com>
2023-10-25 11:19:05 +0800

    删除角色

--- routers/v1/routes/role.go
+++ routers/v1/routes/role.go
@@ -15,8 +15,16 @@ type Role struct {
 
 // 向数据库插入数据
 
-func interRole(role Role) {
-	db.DB.Exec("INSERT INTO roles (name,description,policy) values (?, ?, ?)", role.Name, role.Description, pq.Array(role.Policy))
+func interRole(role Role) error {
+	res := db.DB.Exec("INSERT INTO roles (name,description,policy) values (?, ?, ?)", role.Name, role.Description, pq.Array(role.Policy))
+	return res.Error
+}
+
+// 删除数据库数据
+
+func delRole(id string) error {
+	res := db.DB.Exec("DELETE FROM roles where id = ? ", id)
+	return res.Error
 }
 
 func RetrieveRole(router *gin.RouterGroup) {
@@ -24,12 +32,36 @@ func RetrieveRole(router *gin.RouterGroup) {
 	router.POST("/role", func(ctx *gin.Context) {
 		role := Role{}
 		ctx.ShouldBindJSON(&role)
-		interRole(role)
-		// 获取角色name 权限policy
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-			"role":    role,
-		})
+		err := interRole(role)
+		if err != nil {
+			// 获取角色name 权限policy
+			ctx.JSON(200, gin.H{
+				"message": "角色创建成功",
+				"role":    role,
+			})
+		} else {
+			// 获取角色name 权限policy
+			ctx.JSON(200, gin.H{
+				"message": "角色创建失败",
+				"role":    role,
+			})
+		}
+	})
+	// 删除角色
+	router.DELETE("/role/:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		err := delRole(id)
+		if err != nil {
+			ctx.JSON(200, gin.H{
+				"message": "删除成功",
+				"id":      id,
+			})
+		} else {
+			ctx.JSON(200, gin.H{
+				"message": "删除失败",
+				"id":      id,
+			})
+		}
 	})
 }
 
