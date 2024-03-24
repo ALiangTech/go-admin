a71bdcd1e42da3fdb4677ff52bcb3ccb37686d26
hj <seminapater@gmail.com>
2024-03-24 22:10:03 +0800

    忽略vscode文件夹

--- routers/v1/routes/permission.go
+++ routers/v1/routes/permission.go
@@ -4,7 +4,6 @@ import (
 	"aliangtect/go-admin/routers/types"
 	"aliangtect/go-admin/routers/v1/middlewares"
 	"fmt"
-	"github.com/casbin/casbin/v2"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,13 +12,16 @@ import (
 func RetrievePermission(router *gin.RouterGroup) {
 	router.GET("/permission", func(ctx *gin.Context) {
 		fmt.Println("permission")
-		user, err := casbin.CasbinJsGetPermissionForUser(middlewares.Enforcer, "test")
+		permissions, err := middlewares.Enforcer.GetImplicitPermissionsForUser("test")
 		if err != nil {
 			fmt.Println(err)
-			return
+		}
+		var permissionsCode []string
+		for _, permission := range permissions {
+			permissionsCode = append(permissionsCode, permission[0])
 		}
 		ctx.JSON(200, types.ApiResponse{
-			Data:  user,
+			Data:  permissionsCode,
 			Error: nil,
 		})
 	})
