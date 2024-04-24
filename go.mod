afff440e913aa3db22c683a3506fe91ac515c0cb
HAN <1305544590@qq.com>
2024-04-24 16:28:06 +0800

    用户管理

--- go.mod
+++ go.mod
@@ -2,18 +2,20 @@ module aliangtect/go-admin
 
 go 1.20
 
-require github.com/jinzhu/gorm v1.9.16
+require (
+	github.com/casbin/casbin/v2 v2.77.1
+	github.com/casbin/gorm-adapter/v3 v3.20.0
+	github.com/gin-contrib/cors v1.5.0
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/jinzhu/gorm v1.9.16
+)
 
 require (
 	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
-	github.com/casbin/casbin/v2 v2.77.1 // indirect
-	github.com/casbin/gorm-adapter/v3 v3.20.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/gin-contrib/cors v1.5.0 // indirect
 	github.com/glebarez/go-sqlite v1.20.3 // indirect
 	github.com/glebarez/sqlite v1.7.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
@@ -40,7 +42,7 @@ require (
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.9.1 // indirect
+	github.com/gin-gonic/gin v1.9.1
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.17.0 // indirect
@@ -65,5 +67,5 @@ require (
 	google.golang.org/protobuf v1.32.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/postgres v1.5.2
-	gorm.io/gorm v1.25.4 // indirect
+	gorm.io/gorm v1.25.4
 )
--- routers/v1/routes/user.go
+++ routers/v1/routes/user.go
@@ -1,13 +1,81 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"aliangtect/go-admin/db"
+	"aliangtect/go-admin/routers/types"
+	"fmt"
+	"net/http"
 
-// 查询用户信息
+	"github.com/gin-gonic/gin"
+)
+
+// 查询用户信息结构
+type user struct {
+	Name string `json:"name"`
+}
+
+// 注册用户信息结构
+type register_user struct {
+	Name   string `json:"name" binding:"required"`
+	Pwd    string `json:"pwd" binding:"required"`
+	RoleId int    `json:"roleId" binding:"required"`
+}
 
 func RetrieveUser(router *gin.RouterGroup) {
 	router.GET("/user", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
+		uuid, exists := ctx.Get("userUuid")
+		if exists {
+			var user_info user
+			// uuid 存在则去数据库查询
+			res := db.DB.Raw("select * from users where uuid = ?", uuid).Scan(&user_info)
+			if res.RowsAffected == 1 { // 说明查到了这个uuid 对应的用户数据
+				fmt.Printf("%+v", res.Statement)
+				ctx.JSON(http.StatusOK, types.ApiResponse{
+					Data:  user_info,
+					Error: nil,
+				})
+			} else {
+				ctx.JSON(http.StatusOK, types.ApiResponse{
+					Data: nil,
+					Error: &types.ApiError{
+						Code:    0,
+						Message: "用户不存在",
+					},
+				})
+			}
+		} else {
+			ctx.JSON(http.StatusOK, types.ApiResponse{
+				Data: nil,
+				Error: &types.ApiError{
+					Code:    0,
+					Message: "用户不存在",
+				},
+			})
+		}
 	})
+	// 注册用户
+	router.POST("/user", func(ctx *gin.Context) {
+		//获取表单
+		// todo 判断账号是否存在
+		// 插入数据库
+		register_user_info := register_user{}
+		err := ctx.ShouldBind(&register_user_info)
+		if err == nil {
+			res := db.DB.Exec("INSERT into users (name,pwd,roldid,createdOn) values (?, crypt(?, gen_salt('bf')),?,now())", register_user_info.Name, register_user_info.Pwd, register_user_info.RoleId)
+			if res.RowsAffected == 1 {
+				// 说明插入成功
+				ctx.JSON(http.StatusOK, types.ApiResponse{
+					Data: gin.H{
+						"message": "注册成功",
+					},
+					Error: nil,
+				})
+			}
+		}
+		fmt.Println(err)
+
+	})
+
+	// 删除用户 软删除
+
 }
